ec31597148f69e9dc561e3320442612d4ad52828
Adam Minkus <adam.minkus@gmail..com>
2019-07-26 16:17:39 -0700

    added heroku config

--- server.js
+++ server.js
@@ -32,9 +32,9 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/pdgadb"
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/pdgadb", { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 app.get("/", function(req, res) {
